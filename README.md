b55e10722a6fb6cd357e96c8b6371b304f0d55e5
Super <13424490552@163.com>
2017-12-26 15:12:56 +0800

    切片上传

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPUploadTool
+iOS通过切片仿断点机制上传文件
+<img src="http://images2017.cnblogs.com/blog/912458/201712/912458-20171218170114678-351043689.png" style="max-width:50%" />
---
+++ SPGlobalFile/SPGlobalConst.m
@@ -0,0 +1,14 @@
+//  SPGlobalConst.h
+//
+//  Created by SPSuper on 2017/3/9.
+
+
+NSString *const kSuperUploadFile = @"kSuperUploadFile";
+NSString *const kSuperFinishedUpload = @"kSuperFinishedUpload";
+
+// 测试URL 需要自己配置
+NSString *const kSuperUploadTestUrl = @"http://www.cnblogs.com/chao8888/p/8058457.html";
+
+NSInteger const kSuperUploadBlockSize = 1 * 1024 * 1024;
+
+
