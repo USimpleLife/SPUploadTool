768f6d1409db5ca07e6db4132526735880eaaa9b
Super <13424490552@163.com>
2018-07-31 16:58:00 +0800

    注释

--- SPGlobalFile/SPGlobalConst.m
+++ SPGlobalFile/SPGlobalConst.m
@@ -9,6 +9,7 @@ NSString *const kSuperFinishedUpload = @"kSuperFinishedUpload";
 // 测试URL 需要自己配置
 NSString *const kSuperUploadTestUrl = @"http://120.24.39.25/api/nzp-core-openapi-server/rest/files/MTBPUpload";
 
+// 1M
 NSInteger const kSuperUploadBlockSize = 1 * 1024 * 1024;
 
 
