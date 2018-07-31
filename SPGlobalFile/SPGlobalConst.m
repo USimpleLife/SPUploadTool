5b2ce1cb8584217ac06e240c529cc85a60180c9b
Super <13424490552@163.com>
2018-07-31 16:35:49 +0800

    提供测试上传链接

--- SPGlobalFile/SPGlobalConst.m
+++ SPGlobalFile/SPGlobalConst.m
@@ -7,7 +7,7 @@ NSString *const kSuperUploadFile = @"kSuperUploadFile";
 NSString *const kSuperFinishedUpload = @"kSuperFinishedUpload";
 
 // 测试URL 需要自己配置
-NSString *const kSuperUploadTestUrl = @"配置上传URL";
+NSString *const kSuperUploadTestUrl = @"http://120.24.39.25/api/nzp-core-openapi-server/rest/files/MTBPUpload";
 
 NSInteger const kSuperUploadBlockSize = 1 * 1024 * 1024;
 
